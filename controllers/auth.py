6feeacb96093c9f942f17e58d05f6400474aefbe
Alex-Kael <alex.kaeljunior@gmail.com>
2026-05-31 14:29:52 -0300

    alterada a biblioteca de hash de hashlib para bcrypt

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,8 +1,12 @@
 import streamlit as st
-import hashlib
+import bcrypt
 
 if 'autenticado' not in st.session_state:
     st.session_state['autenticado'] = False
 
-def gerar_hash(senha):
-    return hashlib.sha256(senha.encode('utf-8')).hexdigest()
+def verificar_senha(senha_digitada, hash_do_banco):
+    senha_bytes = senha_digitada.encode('utf-8')
+    hash_bytes = hash_do_banco.encode('utf-8')
+    
+    # Retorna True se a senha estiver correta, False se errada
+    return bcrypt.checkpw(senha_bytes, hash_bytes)
--- views/tela_login.py
+++ views/tela_login.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from database.consultas import buscar_dados_cadastros
-from controllers.auth import gerar_hash
+from controllers.auth import verificar_senha
 
 def renderizar_login():
     with st.container():
@@ -9,20 +9,24 @@ def renderizar_login():
         senha_digitada = st.text_input("Palavra-passe", type="password")
         
         if st.button("Entrar"):
-            # Aplica o hash na senha que o cara acabou de digitar
-            hash_digitado = gerar_hash(senha_digitada)
+            # Puxa as informações do banco de dados primeiro
+            usuario_DB = buscar_dados_cadastros("usuarios", "id_usuario", "usuario")
+            senha_DB = buscar_dados_cadastros("usuarios", "id_usuario", "senha")
             
-            usuario_DB = buscar_dados_cadastros("usuario","id_usuario","usuario")
-            linha_usuario = usuario_DB[0]
-            usuario_banco = linha_usuario[1]
+            if usuario_DB and senha_DB:
+                linha_usuario = usuario_DB[0]
+                usuario_banco = linha_usuario[1]
 
-            senha_DB = buscar_dados_cadastros("usuario","id_usuario","senha")
-            linha_senha = senha_DB[0]
-            hash_banco = linha_senha[1]
-            
-            # Compara o hash gerado com o hash que estava guardado
-            if usuario_digitado == usuario_banco and hash_digitado == hash_banco:
-                st.session_state['autenticado'] = True
-                st.rerun()
+                linha_senha = senha_DB[0]
+                hash_banco = linha_senha[1]
+                
+                # Usa a função do bcrypt para verificar a senha
+                senha_correta = verificar_senha(senha_digitada, hash_banco)
+                
+                if usuario_digitado == usuario_banco and senha_correta:
+                    st.session_state['autenticado'] = True
+                    st.rerun()
+                else:
+                    st.error("Usuário ou senha incorretos.")
             else:
-                st.error("Usuário ou senha incorretos.")
+                st.error("Erro: Nenhum usuário encontrado no banco de dados.")
